177187e834fb2dfa8f45b48ace55ff09594cd79b
en-wai <nyasare@hotmail.com>
2023-04-27 11:16:05 +0000

    Fix arrow alignment in manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Use puppet to create a file in /tmp
 
 file {'/tmp/school':
-  ensure => present,
+  ensure     => present,
   permission => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet'
+  owner      => 'www-data',
+  group      => 'www-data',
+  content    => 'I love Puppet'
 }
