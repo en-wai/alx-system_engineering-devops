9f59128302b5401af9b6531c3c8b4342eb4733b3
en-wai <nyasare@hotmail.com>
2023-04-27 12:10:57 +0000

    Rename resource title to pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Create a file to execute a command
 
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => 'usr/bin',
-  onlyif  => 'pgrep killmenow',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  path     => 'usr/bin',
+  onlyif   => 'pgrep killmenow',
 }
