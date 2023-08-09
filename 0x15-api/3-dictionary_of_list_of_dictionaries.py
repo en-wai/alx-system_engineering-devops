1d5d1ea6372f6dee2db90497596ac3c6103585f5
en-wai <nyasare@hotmail.com>
2023-08-09 22:32:19 +0000

    Write python script to get employee data to Dictionary

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script to export to-do list information of employees to JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employees = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            user.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": user.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": user.get("id")}).json()]
+            for user in employees}, jsonfile)
